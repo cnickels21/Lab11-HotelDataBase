a872439207ce5ccf427d1fbcb692f02d8d2f9466
cnickels21 <cnickels_21@yahoo.com>
2020-05-21 10:14:59 -0500

    hotel room rate data type changed to money

---
+++ Lab12-HotelDataBase/Migrations/20200521151420_ChangedRateTypeToMoney.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab12_HotelDataBase.Migrations
+{
+    public partial class ChangedRateTypeToMoney : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
