d984f70d3e0d5591e9c531ccf8e44e1e78ec5cba
cnickels21 <cnickels_21@yahoo.com>
2020-05-21 20:53:27 -0500

    hotels controller added

---
+++ Lab12-HotelDataBase/Migrations/20200522015020_SlightRefactoring.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab12_HotelDataBase.Migrations
+{
+    public partial class SlightRefactoring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
