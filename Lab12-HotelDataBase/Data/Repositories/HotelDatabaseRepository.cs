412da3385b647f2f8c9cbcf1463f77db6e78b7a4
cnickels21 <cnickels_21@yahoo.com>
2020-05-26 16:21:21 -0500

    interface injected into controller

--- Lab12-HotelDataBase/Data/Repositories/HotelDatabaseRepository.cs
+++ Lab12-HotelDataBase/Data/Repositories/HotelDatabaseRepository.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using Lab12_HotelDataBase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lab12_HotelDataBase.Data.Repositories
 {
-    public class HotelDatabaseRepository
+    public class HotelDatabaseRepository : IHotelRepository
     {
+        private readonly HotelDBContext _context;
+
+        public HotelDatabaseRepository(HotelDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+        public Task<ActionResult<Hotel>> GetHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Hotel>> DeleteHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HotelExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> PutHotel(int id, Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Lab12-HotelDataBase/Data/Repositories/IHotelRepository.cs
+++ Lab12-HotelDataBase/Data/Repositories/IHotelRepository.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Lab12_HotelDataBase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lab12_HotelDataBase.Data.Repositories
 {
-    interface IHotelRepository
+    public interface IHotelRepository
     {
+        Task<ActionResult<IEnumerable<Hotel>>> GetHotels();
+
+        Task<ActionResult<Hotel>> GetHotel(int id);
+
+        Task<IActionResult> PutHotel(int id, Hotel hotel);
+
+        Task<ActionResult<Hotel>> PostHotel(Hotel hotel);
+
+        Task<ActionResult<Hotel>> DeleteHotel(int id);
+
+        Task<bool> HotelExists(int id);
     }
 }
