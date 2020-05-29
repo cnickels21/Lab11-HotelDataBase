10db59162ad52d05b5280632117886afc5262e96
cnickels21 <cnickels_21@yahoo.com>
2020-05-28 22:03:44 -0500

    room views setup

---
+++ Lab12-HotelDataBase.Web/Models/AmenitiesSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab12_HotelDataBase.Web.Models
+{
+    public class AmenitiesSummary
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Lab12-HotelDataBase.Web/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_HotelDataBase.Web.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Layout { get; set; }
+        public List<AmenitiesSummary> Amenities { get; set; }
+    }
+}
