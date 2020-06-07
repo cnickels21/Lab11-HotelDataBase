858b6d805fe8448e5431b19c594a7642bad7694f
cnickels21 <cnickels_21@yahoo.com>
2020-06-06 20:58:42 -0500

    hotel and hotelroom DTOs defined

--- Lab12-HotelDataBase/Models/Api/HotelDTO.cs
+++ Lab12-HotelDataBase/Models/Api/HotelDTO.cs
@@ -7,5 +7,12 @@ namespace Lab12_HotelDataBase.Models.Api
 {
     public class HotelDTO
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public List<HotelRoomDTO> Rooms { get; set; }
     }
 }
