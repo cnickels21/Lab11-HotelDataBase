a4049ecb9a7b4710995094b5c44c392f99630938
cnickels21 <cnickels_21@yahoo.com>
2020-06-06 20:06:05 -0500

    home controller refactor

--- Lab12-HotelDataBase.Web/Controllers/HomeController.cs
+++ Lab12-HotelDataBase.Web/Controllers/HomeController.cs
@@ -31,7 +31,14 @@ namespace Lab12_HotelDataBase.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel 
+            { 
+                RequestId = Activity.Current?.Id ?? 
+                HttpContext.TraceIdentifier 
+            });
         }
+
+
+
     }
 }
