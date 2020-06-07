2f0d142413caeabe35c00fd8ff6e8466d982cce9
cnickels21 <cnickels_21@yahoo.com>
2020-06-06 21:28:34 -0500

    hotel room composite key redefined

--- Lab12-HotelDataBase/Migrations/20200607022739_HotelRoomPrimaryCompositeKey.Designer.cs
+++ Lab12-HotelDataBase/Migrations/20200607022739_HotelRoomPrimaryCompositeKey.Designer.cs
@@ -3,14 +3,16 @@ using Lab12_HotelDataBase.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lab12_HotelDataBase.Migrations
 {
     [DbContext(typeof(HotelDBContext))]
-    partial class HotelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200607022739_HotelRoomPrimaryCompositeKey")]
+    partial class HotelRoomPrimaryCompositeKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
